f4325e34684f9cc362910f7a6ce6327aefeb5ab3
Michael Neidhardt <mneidhardt@gmail.com>
2015-04-18 09:49:34 +0200

    Initial Commit

---
+++ Triangulate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Triangulate
+//
+//  Created by michael neidhardt on 18/04/15.
+//  Copyright (c) 2015 michael neidhardt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
